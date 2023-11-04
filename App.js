d270a8c68c238467205004916ae50a213cf54327
shubhu1026 <shubhu.p2000@gmail.com>
2023-11-04 14:06:42 -0400

    Application completed

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import SearchScreen from "./src/screens/SearchScreen.js";
+import ResultsShowScreen from "./src/screens/ResultsShowScreen.js";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
+    ResultsShow: ResultsShowScreen,
   },
   {
     initialRouteName: "Search",
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { withNavigation } from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
@@ -13,7 +23,15 @@ const ResultsList = ({ title, results }) => {
         showsHorizontalScrollIndicator={false}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />;
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("ResultsShow", { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
@@ -32,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
